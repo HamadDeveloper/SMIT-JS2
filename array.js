2196d47768197f4da07adfb758e240cd15c498c1
Hamad Ur Rehman <ceerhamad@gmail.com>
2023-11-26 14:40:41 +0500

    loop code are added

---
+++ array.js
@@ -0,0 +1,19 @@
+var student = ['hamad','aslam','akram','jinah']
+console.log(student.length)
+console.log(student[1])
+
+// replacing a name
+
+student[2] = 'yasir';
+console.log(student)
+
+// CRUD
+ console.log (student.pop());
+ console.log(student)
+
+ // shift or unshift work like  a pop and push  but it the start of the array
+
+ student.shift();
+  var teacher = ['shaukat','asim jalal','ibrar']
+
+  teacher.push('allahdad',)
