1714049f0697027f6b0a8bcb7c016273bc0e4867
Hamad Ur Rehman <ceerhamad@gmail.com>
2023-11-26 14:50:51 +0500

    updated

--- loops.js
+++ loops.js
@@ -1,6 +1,11 @@
 // for loop
 
-for( let i=0; i<10; i++){
-console.log(i,"Hamad Ur Rehman");
-console.log('*');
+//for( let i=0; i<10; i++){
+//console.log(i,"Hamad Ur Rehman");
+//console.log('*');
+//}
+
+
+for (let a = 0; a <= 10; a = a+2){
+    console.log(a,'Hamad');
 }
