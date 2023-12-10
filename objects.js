8f3258ebd1a98348bfbe0df67a91bb6d58520efc
Hamad Ur Rehman <114566974+HamadDeveloper@users.noreply.github.com>
2023-12-10 09:40:16 +0500

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,15 @@
+// var person = {
+//     name : 'Hamad',
+//     age : 23,
+//     gender : 'Male',
+// }
+
+// console.log(person);
+
+var Fruits = {
+    color : 'red',
+    taste : 'sweet',
+    season : 'spring',
+}
+
+console.log(Fruits);
