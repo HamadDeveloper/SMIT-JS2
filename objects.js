d5c164b7a7c2353436b817757c65b20d33e14c5d
Hamad Ur Rehman <ceerhamad@gmail.com>
2023-12-10 14:33:29 +0500

    array of objects

--- objects.js
+++ objects.js
@@ -6,22 +6,43 @@
 
 // console.log(person);
 
-var Fruits = {
-    color : 'red',
-    taste : 'sweet',
-    season : 'spring',
+// var Fruits = {
+//     color : 'red',
+//     taste : 'sweet',
+//     season : 'spring',
+// }
+
+
+
+
+// var Patient = {
+//     id : '10',
+//     name : 'Asad',
+//     desease : 'Sugar',
+//     hospital : 'RMI',
+// }
+//  delete Patient.hospital;
+// console.log(Patient);
+
+// console.log(Fruits);
+
+var Patient1 = {id : '1', name : 'Hamad', desease : 'TB',}
+var Patient2 = {id : '11', name : 'asad', desease : 'moye moye',}
+var Patient3 = {id : '12', name : 'aslam', desease : 'typhoid',}
+var Patient4 = {id : '13', name : 'waqas', desease : 'flu',}
+var Patient5 = {id : '14', name : 'ali', desease : 'flu',}
+var Patient6 = {id : '15', name : 'waqas', desease : 'moye moye',}
+var Patient7 = {id : '16', name : 'jibran', desease : 'sugar',}
+var Patient8 = {id : '17', name : 'khan', desease : 'aids',}
+
+let patients = [Patient1,Patient2,Patient3,Patient4,Patient5,Patient6,Patient7,Patient7];
+
+for (let i = 0 ; i<=patients.length; i++){
+    for (key in patients[i]){
+        if(key == 'desease'){
+            if (patients[i][key] == 'moye moye'){
+                console.log(patients[i][key], patients[i]['name'])
+            }
+        }
+    }
 }
-
-
-
-
-var Patient = {
-    id : '10',
-    name : 'Asad',
-    desease : 'Sugar',
-    hospital : 'RMI',
-}
- delete Patient.hospital;
-console.log(Patient);
-
-console.log(Fruits);
